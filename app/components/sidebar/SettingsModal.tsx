0acec15f58b0a23f87065ce93868b175ec5cff3f
Tunahan Dogan <piyealar@gmail.com>
2023-06-04 17:29:19 +0300

    ezilmem yıkılmam

--- app/components/sidebar/SettingsModal.tsx
+++ app/components/sidebar/SettingsModal.tsx
@@ -8,7 +8,7 @@ import { User } from '@prisma/client'
 import { CldUploadButton } from 'next-cloudinary'
 
 import Input from '../inputs/Input'
-import Modal from '../modals/Modal'
+import Modal from '../Modal'
 import Button from '../Button'
 import Image from 'next/image'
 import { toast } from 'react-hot-toast'
